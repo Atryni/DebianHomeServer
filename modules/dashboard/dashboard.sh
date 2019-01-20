c525a64d47e4f95eeddf5bba0c0ce2c802a7f1dd
Przemysław Sawczuk <przemeksawczuk@yahoo.pl>
2019-01-21 00:06:29 +0100

    #2

--- modules/dashboard/dashboard.sh
+++ modules/dashboard/dashboard.sh
@@ -4,10 +4,11 @@ echo "The present working directory is `pwd`"
 
 DIR=`dirname "$0"`
 
+sudo mkdir /var/www/dashboard.rpi
 git clone https://github.com/BlackrockDigital/startbootstrap-freelancer.git /var/www/dashboard.rpi
 
 cp ${DIR}/index.html /var/www/dashboard.rpi/
-cp ${DIR}/dashboard.rpi.conf /etc/apache2/sites-available/
-ln -s /etc/apache2/sites-available/dashboard.rpi.conf /etc/apache2/sites-enabled/dashboard.rpi.conf
+sudo cp ${DIR}/dashboard.rpi.conf /etc/apache2/sites-available/
+sudo ln -s /etc/apache2/sites-available/dashboard.rpi.conf /etc/apache2/sites-enabled/dashboard.rpi.conf
 rm /var/www/html -Rf
 ln -s /var/www/dashboard.rpi /var/www/html
